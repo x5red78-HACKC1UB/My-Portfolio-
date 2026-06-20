f6e8ac87bbeacfb73c16ff7405cec25ad158998e
x5red78-HACKC1UB <x5red78@gmail.com>
2026-06-20 18:27:51 +0000

    made the buttons work

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
 const codingButton = document.getElementById("codeprojects");
+const githubrepo= document.getElementById("githubrepo");
+const actualdemo = document.getElementById("actualweb");
+const githubrepo1= document.getElementById("githubrepo1");
+const actualdemo1 = document.getElementById("actualweb1");
+
+githubrepo.addEventListener("click",()=>{
+window.location.href="https://github.com/x5red78-HACKC1UB/money-cliker"
+});
+actualdemo.addEventListener("click", () =>{
+window.location.href="https://x5red78-hackc1ub.github.io/money-cliker/"
+});
+githubrepo1.addEventListener(click,()=>{
+window.location.href="https://github.com/x5red78-HACKC1UB/password-generator"
+});
+actualdemo.addEventListener("click", () =>{
+window.location.href="https://x5red78-hackc1ub.github.io/password-generator/"
+});
+
+
+
+
+
+
+
+
+
+
 
 codingButton.addEventListener("click", () => {
   window.location.href="coding.html";
