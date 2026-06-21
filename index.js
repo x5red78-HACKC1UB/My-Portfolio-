c691b88497e58e4060982c894db37e03a9633f51
x5red78-HACKC1UB <x5red78@gmail.com>
2026-06-21 23:17:44 +0000

    fixed a bug

--- index.js
+++ index.js
@@ -3,31 +3,49 @@ const githubrepo= document.getElementById("githubrepo");
 const actualdemo = document.getElementById("actualweb");
 const githubrepo1= document.getElementById("githubrepo1");
 const actualdemo1 = document.getElementById("actualweb1");
-
+const lightdark= document.getElementById("light-dark");
+const back= document.getElementById("back")
+if(githubrepo){
 githubrepo.addEventListener("click",()=>{
 window.location.href="https://github.com/x5red78-HACKC1UB/money-cliker"
 });
-actualdemo.addEventListener("click", () =>{
+};
+if (actualdemo) {
+  
+  actualdemo.addEventListener("click", () =>{
 window.location.href="https://x5red78-hackc1ub.github.io/money-cliker/"
 });
-githubrepo1.addEventListener(click,()=>{
+
+}
+
+if (githubrepo1) {
+  githubrepo1.addEventListener("click",()=>{
 window.location.href="https://github.com/x5red78-HACKC1UB/password-generator"
 });
-actualdemo.addEventListener("click", () =>{
+};
+if(actualdemo1){
+actualdemo1.addEventListener("click", () =>{
 window.location.href="https://x5red78-hackc1ub.github.io/password-generator/"
 });
+};
+
+if (back) {
+  
+
+back.addEventListener(click, ()=>{
+window.location.href="index.html"
+});
+
+}
 
 
 
 
 
-
-
-
-
-
+if (codingButton) {
+  
 
 codingButton.addEventListener("click", () => {
   window.location.href="coding.html";
 });
-
+};
